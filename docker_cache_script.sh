16e521b164b06fb7b266a1ae39a96c1c13f716a3
Suigetsu <lagmed.019@gmail.com>
2024-09-12 11:16:12 +0100

    fixed linter issues, changed permissions too

--- docker_cache_script.sh
+++ docker_cache_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/goinfre/docker" ]; then
-    mkdir $HOME/goinfre/docker
-    rm -rf $HOME/Library/Containers/com.docker.docker
-    ln -s $HOME/goinfre/docker $HOME/Library/Containers/com.docker.docker
+if [ ! -d ""$HOME"/goinfre/docker" ]; then
+    mkdir "$HOME"/goinfre/docker
+    rm -rf "$HOME"/Library/Containers/com.docker.docker
+    ln -s "$HOME"/goinfre/docker "$HOME"/Library/Containers/com.docker.docker
     echo "The docker directory has been created and the cache has been cleared."
 else
     echo "The cache is already cleared and the goinfre directory exists!"
